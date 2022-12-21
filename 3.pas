cd3f1252d326b71c06192f203aea153e9811da4b
Alice <119490190+Alisyyyn@users.noreply.github.com>
2022-12-21 21:34:00 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,27 @@
+﻿var F_in,F_out: Text;
+  Name,S: String;
+begin
+ Assign(F_in,'D:\text.txt');
+ Reset(F_in);
+ Assign(F_out,'D:\text1.txt');
+ Rewrite(F_out);
+ While not eof(F_in) do
+  begin
+   Readln(F_in,S);
+   Writeln(F_out,S);
+  end;
+  Write('Введите строку:');
+ Readln(S);
+  Writeln(F_out,S);
+  Close(F_in);
+ Close(F_out);
+ Rewrite(F_in);
+ Reset(F_out);
+ While not eof(F_out) do
+  begin
+   Readln(F_out,S);
+   Writeln(F_in,S);
+  end;
+ Close(F_in);
+ Close(F_out);
+end.
